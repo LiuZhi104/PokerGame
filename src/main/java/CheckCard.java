5ff60a9b2b9b5cf87e6e4ff6f995385e045d7a39
LiuZhi104 <2458461623@qq.com>
2019-08-18 13:20:23 +0800

    由testTwoPoker测试，驱动出checkCard的排序最大值方法，改变getWinner方法，改变以后两个测试都通过

--- src/main/java/CheckCard.java
+++ src/main/java/CheckCard.java
@@ -1,11 +1,45 @@
-public class CheckCard {
-    public static String getWinner(Player player1, Player player2) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-        int palyer1Value = Integer.valueOf(player1.getCardGroup().get(0).getCardValue());
-        int palyer2Value = Integer.valueOf(player2.getCardGroup().get(0).getCardValue());
-        if(palyer1Value>palyer2Value){
+public class CheckCard {
+
+    public static String getWinner(Player player1, Player player2) {
+        int palyer1Value = getMaxValue(player1);
+        int palyer2Value = getMaxValue(player2);
+        if (palyer1Value > palyer2Value) {
             return player1.getName();
         }
         return player2.getName();
+    }
+
+    public  static  int transformValue(String cardVlue) {
+        if (cardVlue == "T") {
+            return 10;
+        }
+        if (cardVlue == "J") {
+            return 11;
+        }
+        if (cardVlue == "Q") {
+            return 12;
+        }
+        if (cardVlue == "K") {
+            return 13;
+        }
+        if (cardVlue == "A") {
+            return 14;
+        }
+        return Integer.valueOf(cardVlue);
+    }
+
+    public static int getMaxValue(Player player) {
+        List<Integer> cardGroupValues = new ArrayList<>();
+        for (int i = 0; i < player.getCardGroup().size(); i++) {
+            cardGroupValues.add(transformValue(player.getCardGroup().get(i).getCardValue()));
+        }
+        Integer[] cardValues = cardGroupValues.toArray(new Integer[cardGroupValues.size()]);
+        Arrays.sort(cardValues);
+        return cardValues[cardValues.length - 1];
+
     }
 }
