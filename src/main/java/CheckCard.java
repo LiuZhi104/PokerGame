a054eac367d773f7ac69bd6467a1dcd7de1fb625
LiuZhi104 <2458461623@qq.com>
2019-08-18 13:33:39 +0800

    修改并在checkCard添加方法，测试通过

--- src/main/java/CheckCard.java
+++ src/main/java/CheckCard.java
@@ -5,12 +5,10 @@ import java.util.List;
 public class CheckCard {
 
     public static String getWinner(Player player1, Player player2) {
-        int palyer1Value = getMaxValue(player1);
-        int palyer2Value = getMaxValue(player2);
-        if (palyer1Value > palyer2Value) {
-            return player1.getName();
-        }
-        return player2.getName();
+        Integer[] palyer1Values = getSortValues(player1);
+        Integer[] palyer2Values = getSortValues(player2);
+        return compareCardValues(palyer1Values,palyer2Values);
+
     }
 
     public  static  int transformValue(String cardVlue) {
@@ -32,14 +30,24 @@ public class CheckCard {
         return Integer.valueOf(cardVlue);
     }
 
-    public static int getMaxValue(Player player) {
+    public static Integer[] getSortValues(Player player) {
         List<Integer> cardGroupValues = new ArrayList<>();
         for (int i = 0; i < player.getCardGroup().size(); i++) {
             cardGroupValues.add(transformValue(player.getCardGroup().get(i).getCardValue()));
         }
         Integer[] cardValues = cardGroupValues.toArray(new Integer[cardGroupValues.size()]);
         Arrays.sort(cardValues);
-        return cardValues[cardValues.length - 1];
+        return cardValues;
+    }
 
+    public static String compareCardValues( Integer[] palyer1Values, Integer[] palyer2Values){
+        for(int i = palyer1Values.length-1 ;i >= 0 ;i--){
+            if(palyer1Values[i]>palyer2Values[i]){
+                return "palyer1";
+            }else if(palyer1Values[i] < palyer2Values[i]){
+                return "palyer2";
+            }
+        }
+        return "Equal value";
     }
 }
