0435f179e4ca07944338bb9d1df7a3c45b17b10f
LiuZhi104 <2458461623@qq.com>
2019-08-18 15:02:22 +0800

    添加相同的顺序但是花色不同的牌的Test，测试通过

--- src/test/java/PokerTest.java
+++ src/test/java/PokerTest.java
@@ -278,6 +278,26 @@ public class PokerTest {
         //then
         Assert.assertEquals("palyer2", winner);
     }
+    @Test
+    public void testFivePoker_same_straight_and_diff_color() {
+//        palyer1 3H 4D 5S 6C 7D
+//        palyer2 3S 4H 5D 6H 7C
+        //given
+        Player player1 = new Player("palyer1");
+        Player player2 = new Player("palyer2");
+        player1.setCardGroup(Arrays.asList(new Poker("H", "3"), new Poker("D", "4"),
+                new Poker("S", "5"), new Poker("C", "6"), new Poker("D", "7")));
+        player1.computeCardLevel();
+        player2.setCardGroup(Arrays.asList(new Poker("S", "3"), new Poker("H", "4"),
+                new Poker("D", "5"), new Poker("H", "6"), new Poker("C", "7")));
+        player2.computeCardLevel();
+
+        //when
+        String winner = CheckCard.getWinner(player1, player2);
+
+        //then
+        Assert.assertEquals("Equal value", winner);
+    }
 
 
 
