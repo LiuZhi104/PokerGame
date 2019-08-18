0d3f03f3e93f73515d633ea7ffecf3f5858da6cc
LiuZhi104 <2458461623@qq.com>
2019-08-18 13:24:04 +0800

    由testFivePoker_and_no_pair_is_easiest测试，测试通过

--- src/test/java/PokerTest.java
+++ src/test/java/PokerTest.java
@@ -34,6 +34,26 @@ public class PokerTest {
         //then
         Assert.assertEquals("palyer1",winner);
 
+    }
+
+    @Test
+    public void testFivePoker_and_no_pair_is_easiest(){
+//        palyer1 2H 3D 5S 9C KD
+//        palyer2 2H 3D 5S 9C QD
+        //given
+        Player player1 = new Player("palyer1");
+        Player player2 = new Player("palyer2");
+        player1.setCardGroup(Arrays.asList(new Poker("H","2"),new Poker("D","3"),
+                new Poker("s","5"),new Poker("C","9"),new Poker("D","K") ));
+        player2.setCardGroup(Arrays.asList(new Poker("H","2"),new Poker("D","3"),
+                new Poker("s","5"),new Poker("C","9"),new Poker("D","Q") ));
+
+        //when
+        String winner =  CheckCard.getWinner(player1,player2);
+
+        //then
+        Assert.assertEquals("palyer1",winner);
 
     }
+
 }
