5128e7d90bf6e532483dc199e59f34af53a22aed
LiuZhi104 <2458461623@qq.com>
2019-08-18 13:16:34 +0800

    比较两张牌的测试用例完成，测试失败

--- src/test/java/PokerTest.java
+++ src/test/java/PokerTest.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 
 public class PokerTest {
     @Test
@@ -16,6 +18,22 @@ public class PokerTest {
 
         //then
         Assert.assertEquals("palyer2",winner);
+    }
+
+    @Test
+    public void testTwoPoker(){
+        //given
+        Player player1 = new Player("palyer1");
+        Player player2 = new Player("palyer2");
+        player1.setCardGroup(Arrays.asList(new Poker("H","6"),new Poker("H","A")));
+        player2.setCardGroup(Arrays.asList(new Poker("H","9"),new Poker("C","8")));
+
+        //when
+        String winner =  CheckCard.getWinner(player1,player2);
+
+        //then
+        Assert.assertEquals("palyer1",winner);
+
 
     }
 }
